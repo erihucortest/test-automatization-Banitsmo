79bfebf2d5c71196ef93a10f7f35e18c2a04997c
Erick Cordova G <erihucor@gmail.com>
2025-02-10 10:41:26 -0500

    ECG: add user

---
+++ src/test/java/mypackage/User.java
@@ -0,0 +1,10 @@
+package mypackage;
+
+import net.serenitybdd.screenplay.Actor;
+
+public class User {
+
+    public static Actor actor() {
+        return Actor.named("Nohelya Borrero Arias");
+    }
+}
